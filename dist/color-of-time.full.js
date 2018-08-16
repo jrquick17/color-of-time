6f65326d017afa5aedbc750d8980e3bda9057fd2
“Jeremy <jrquick@uab.edu>
2018-08-16 15:05:48 -0500

    Slit into multiple functions

--- dist/color-of-time.full.js
+++ dist/color-of-time.full.js
@@ -76,51 +76,62 @@
     function ColorOfTimeService() {
         var ColorOfTimeService = this;
 
-        ColorOfTimeService.getColor = function(speed) {
-            if (typeof speed === 'undefined') {
-                speed = 1;
-            }
+        ColorOfTimeService.getColor = function() {
+            var remainingPercent = ColorOfTimeService._getRemainingDayPercent();
 
-            //Calculate % of day already passed
-            var date = new Date();
-            var hour = (date.getHours() * 60 * 60 + date.getMinutes() * 60 + date.getSeconds()) / 86400;
-            hour = hour * speed % 1;
+            return ColorOfTimeService._getColorPercent(remainingPercent);
+        };
 
-            //Calculate color value [0-255] based on the time
+        ColorOfTimeService._getColorPercent = _getColorPercent;
+        function _getColorPercent(percent) {
             var red = 0;
             var green = 0;
             var blue = 0;
 
-            if (hour < 0.17) {
+            if (percent < 0.17) {
                 red = 1;
-                green = hour / 0.17;
+                green = percent / 0.17;
                 blue = 0;
-            } else if (hour < 0.34) {
-                red = 1 - ((hour - 0.17) / 0.17);
+            } else if (percent < 0.34) {
+                red = 1 - ((percent - 0.17) / 0.17);
                 green = 1;
                 blue = 0;
-            } else if (hour < 0.51) {
+            } else if (percent < 0.51) {
                 red = 0;
                 green = 1;
-                blue = (hour - 0.34) / 0.17;
-            } else if (hour < 0.68) {
+                blue = (percent - 0.34) / 0.17;
+            } else if (percent < 0.68) {
                 red = 0;
-                green = 1 - ((hour - 0.51) / 0.17);
+                green = 1 - ((percent - 0.51) / 0.17);
                 blue = 1;
-            } else if (hour < 0.85) {
-                red = (hour - 0.68) / 0.17;
+            } else if (percent < 0.85) {
+                red = (percent - 0.68) / 0.17;
                 green = 0;
                 blue = 1;
-            } else if (hour <= 1) {
+            } else if (percent <= 1) {
                 red = 1;
                 green = 0;
-                blue = 1 - ((hour - 0.85) / 0.15);
+                blue = 1 - ((percent - 0.85) / 0.15);
             }
 
-            //Convert color value [0-255] to hexadecimal
-            red = Math.floor(red * 255).toString(16);
-            green = Math.floor(green * 255).toString(16);
-            blue = Math.floor(blue * 255).toString(16);
+            return ColorOfTimeService._toHexadecimal(red, blue, green);
+        }
+
+        ColorOfTimeService._getRemainingDayPercent = _getRemainingDayPercent;
+        function _getRemainingDayPercent() {
+            var date = new Date();
+
+            var hour = (date.getHours() * 60 * 60 + date.getMinutes() * 60 + date.getSeconds()) / 86400;
+
+            return hour % 1;
+        }
+
+        ColorOfTimeService._toHexadecimal = _toHexadecimal;
+        function _toHexadecimal(red, blue, green) {
+            red = _to255(red, blue, green);
+            green = _to255(green);
+            blue = _to255(blue);
+
             if (red.length === 1) {
                 red = "0".concat(red);
             }
@@ -132,6 +143,11 @@
             }
 
             return "#" + red.concat(green).concat(blue).toUpperCase();
-        };
+        }
+
+        ColorOfTimeService._to255 = _to255;
+        function _to255(value) {
+            return Math.floor(value * 255).toString(16);
+        }
     }
 })();
